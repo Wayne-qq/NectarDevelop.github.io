5ef1b64a11425a05500cb40a2434919772f8f3c3
Wayne-qq <153057848+Wayne-qq@users.noreply.github.com>
2024-01-10 23:04:01 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+// // script.js
+// const textElement = document.querySelector('.main__page h1');
+
+// anime.timeline({ loop: false })
+//   .add({
+//     targets: '.main__page h1',
+//     translateY: [-500, 0],
+//     opacity: [0, 1],
+//     easing: 'easeOutExpo',
+//     duration: 2000,
+//     delay: (el, i) => 200 * i
+//   });
